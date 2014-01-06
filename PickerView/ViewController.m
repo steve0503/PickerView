c17f3f1efa01337608d9ee588d8cce3b37f9773c
steve0503 <leehr76@gmail.com>
2014-01-06 14:06:51 +0900

    PickerView
    Done

--- PickerView/ViewController.m
+++ PickerView/ViewController.m
@@ -8,16 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPickerViewDelegate, UIPickerViewDataSource>{
+    
+    NSArray *data;
+    
+    
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
 
 @end
 
 @implementation ViewController
 
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    return 2;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    
+    if (0 == component) {
+        return 10;
+    }
+    else
+        return [data count];
+    
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    
+    if (0 == component) {
+        return [NSString stringWithFormat:@" Row: %ld",row];
+    }
+    else{
+    
+        return [data objectAtIndex:row];
+    }
+    
+    
+    
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    
+    NSString *item = [self pickerView:pickerView titleForRow:row forComponent:component];
+    
+    self.label.text = [NSString stringWithFormat:@"컴포넌트 %ld 로우 %ld 항목 : %@",component,row,item];
+    
+}
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    data = [[NSArray alloc] initWithObjects:@"Item1",@"Item2",@"Item3",@"Item4",@"Item5", nil];
 }
 
 - (void)didReceiveMemoryWarning
