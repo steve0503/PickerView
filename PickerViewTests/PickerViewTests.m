14b424f0264407eaff49098abb241a7123270ab8
steve0503 <leehr76@gmail.com>
2014-01-06 13:48:26 +0900

    Initial Commit

---
+++ PickerViewTests/PickerViewTests.m
@@ -0,0 +1,34 @@
+//
+//  PickerViewTests.m
+//  PickerViewTests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PickerViewTests : XCTestCase
+
+@end
+
+@implementation PickerViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
